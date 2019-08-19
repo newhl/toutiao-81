f06785296acec4fcc4857f298b6ff34190877b70
hl <1813927324@qq.com>
2019-08-19 22:54:33 +0800

    登录模块-数据绑定及校验及安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入elementUI的样式
 import './styles/index.less' // 引入默认样式
+import axios from 'axios'
 
+Vue.prototype.$axios = axios // 把axios的插件给了vue的原型属性
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将后台的地址给到axios的baseUrl
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(axios)
 
 new Vue({
   router,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,19 +5,20 @@
               <img src="../../assets/img/logo_index.png" alt="">
           </div>
         <!-- 表单结构 -->
-          <el-form>
-              <el-form-item>
-                  <el-input placeholder="请输入手机号"></el-input>
+          <el-form :model="formData" :rules="rules" ref="loginForm">
+              <!-- el-from-item的prop属性绑定需要校验的字段 -->
+              <el-form-item prop="mobile">
+                  <el-input  v-model="formData.mobile" placeholder="请输入手机号"></el-input>
               </el-form-item>
-              <el-form-item>
-                  <el-input placeholder="验证码" style="width:65%"></el-input>
+              <el-form-item prop="code">
+                  <el-input  v-model="formData.code" placeholder="验证码" style="width:65%"></el-input>
                   <el-button type="primary" style="float:right">发送验证码</el-button>
               </el-form-item>
-              <el-form-item>
-                  <el-checkbox v-model="checked">我已阅读用户协议</el-checkbox>
+              <el-form-item prop="check">
+                  <el-checkbox v-model="formData.check">我已阅读用户协议</el-checkbox>
               </el-form-item>
               <el-form-item>
-                  <el-button style="width:100%" type="warning" round>登录</el-button>
+                  <el-button @click="login" style="width:100%" type="warning" round>登录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
@@ -26,6 +27,49 @@
 
 <script>
 export default {
+  // 定义表单数据对象
+  data () {
+    var func = function (rule, value, callbcak) {
+      if (value) {
+        // 满足校验
+        callbcak()
+      } else {
+        callbcak(new Error('错误信息'))
+      }
+    }
+    return {
+      formData: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式错误', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码长度必须为6个数字', trigger: 'blur' }
+        ],
+        check: [
+          { message: '验证码不能为空', validator: func }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 手动校验表单的数据
+      // 是否通过了当前的校验结果
+      // result当前的校验结果
+      this.$refs.loginForm.validate(isOK => {
+        if (isOK) {
+          console.log('成功')
+        }
+      })
+    }
+  }
 
 }
 </script>
