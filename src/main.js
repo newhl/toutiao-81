bf072868d15c568ffa9ca0bf18737b39a33e23bf
hl <1813927324@qq.com>
2019-08-20 21:55:02 +0800

    全局注册插件的使用方式Vue.use

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import Component from './components'
 import 'element-ui/lib/theme-chalk/index.css' // 引入elementUI的样式
 import './styles/index.less' // 引入默认样式
 import axios from 'axios'
@@ -10,6 +11,7 @@ Vue.prototype.$axios = axios // 把axios的插件给了vue的原型属性
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将后台的地址给到axios的baseUrl
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(Component) // 注册全局
 // Vue.use(axios)
 
 new Vue({
