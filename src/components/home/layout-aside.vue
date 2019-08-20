3c7f859d236cd34736e55b9a2be4f15424997a75
hl <1813927324@qq.com>
2019-08-20 19:19:04 +0800

    主页模块-主页布局

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout-aside">
+      <div class="title">
+          <img src="../../assets/img/logo_admin.png" alt="">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+    .layout-aside{
+        .title{
+
+        }
+    }
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>这是home</div>
+<!-- 主体容器 -->
+  <el-container>
+    <!-- 左侧边栏 -->
+    <el-aside>
+      <layout-asidee></layout-asidee>
+    </el-aside>
+    <el-container>
+      <!-- 头部内容 -->
+    <el-header>Header</el-header>
+    <!-- 主体内容 -->
+    <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
+
+  component: {
+    'layout-aside': layoutAside
+  }
 
 }
 </script>
