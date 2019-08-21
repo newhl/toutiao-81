7aceb9263d0a559856e34735f3dec265973cd47d
hl <1813927324@qq.com>
2019-08-21 15:58:21 +0800

    主页模块-默认导航

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home'
 import Login from './views/login/index.vue'
+import Main from './views/home/main.vue'
 
 Vue.use(Router)
 
@@ -19,7 +20,13 @@ export default new Router({
     {
       path: '/home',
       name: 'home',
-      component: Home
+      component: Home,
+      children: [
+        {
+          path: '',
+          component: Main
+        }
+      ]
     }
     // {
     //   path: '/about',
