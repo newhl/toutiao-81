96bd5800b86a7fd6f1feabba52927efb0a94dd9c
hl <1813927324@qq.com>
2019-08-24 13:14:15 +0800

    页面结构-请求数据

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -37,35 +37,36 @@
         ></el-date-picker>
       </el-form-item>
     </el-form>
-    <div class="total-title">
-        共找到56789符合条件的内容
-    </div>
+    <div class="total-title">共找到{{page.total}}条数据</div>
     <!-- 内容列表 -->
     <div class="content-list">
-        <div class="content-item" v-for="(item,index) in list" :key="index">
-            <!-- 左侧内容 -->
-            <div class="left">
-                <img src="" alt="">
-                <div class="info">
-                    <span>我是标题</span>
-                    <el-tag>标签一</el-tag>
-                    <span class="date">15165165</span>
-                </div>
-            </div>
-            <!-- 右侧内容 -->
-            <div class="right">
-                <span>
-                    <i class="el-icon-edit"></i>
-                    修改
-                </span>
-                <span>
-                    <i class="el-icon-delete"></i>
-                    删除
-                </span>
-            </div>
+      <div class="content-item" v-for="(item,index) in list" :key="index">
+        <!-- 左侧内容 -->
+        <div class="left">
+          <img src="../../assets/img/404.png" alt="" />
+          <div class="info">
+            <span>{{item.title}}</span>
+            <el-tag style="width:60px" :type="item.status | statusType">{{ item.status | statusText }}</el-tag>
+            <span class="date">{{item.pubdate}}</span>
+          </div>
         </div>
+        <!-- 右侧内容 -->
+        <div class="right">
+          <span>
+            <i class="el-icon-edit"></i>
+            修改
+          </span>
+          <span>
+            <i class="el-icon-delete"></i>
+            删除
+          </span>
+        </div>
+      </div>
     </div>
 
+    <el-row type="flex" justify="center" style="margin:20px 0">
+      <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -98,54 +99,99 @@ export default {
           label: '北京烤鸭'
         }
       ],
-      list: [1, 2, 3, 4, 5, 6, 7] // 内容列表
+      list: [], // 内容列表
+      page: {
+        total: 0
+      }
     }
+  },
+  methods: {
+    getArticles () {
+      this.$axios({
+        url: '/articles'
+      }).then(result => {
+        this.list = result.data.results
+        this.page.total = result.data.total_count //
+      })
+    }
+  },
+  //
+  filters: {
+    statusText (value) {
+      switch (value) {
+        case 0:
+          return '草稿'
+        case 2:
+          return '已发表'
+        case 3:
+          return '审核失败'
+        case 4:
+          return '已删除'
+      }
+    },
+    // 过滤tab标签的类型
+    statusType (value) {
+      switch (value) {
+        case 0:
+          return 'waring'
+        case 2:
+          return 'success'
+        case 3:
+          return 'danger'
+        case 4:
+          return 'info'
+      }
+    }
+  },
+  created () {
+    this.getArticles()
   }
 }
 </script>
 
 <style lang="less" scoped>
-.total-title{
-    height: 60px;
-    line-height: 60px;
-    border-bottom: 1px dashed #ccc;
+.total-title {
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px dashed #ccc;
 }
-    .content-list{
-       .content-item{
-           display: flex;
-           justify-content: space-between;
-           padding: 10px;
-           border-bottom: 1px solid  #f2f3f5;
-           .left{
-               display: flex;
-               align-items: center;
-               img{
-                   width: 150px;
-                   height: 100px;
-                   border-radius: 4px;
-               }
-               .info{
-                   margin-left: 10px;
-                   height: 100px;
-                   padding: 5px 0;
-                   display: flex;
-                   flex-direction: column;
-                   justify-content: space-between;
-                   .date{
-                       font-size: 12px;
-                       color: #999;
-                   }
-               }
-           }
-           .right{
-               span, span i {
-                   font-size: 12px;
-                   color: #333;
-               }
-               span{
-                   margin-right: 5px
-               }
-           }
-       }
+.content-list {
+  .content-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    border-bottom: 1px solid #f2f3f5;
+    .left {
+      display: flex;
+      align-items: center;
+      img {
+        width: 150px;
+        height: 100px;
+        border-radius: 4px;
+      }
+      .info {
+        margin-left: 10px;
+        height: 100px;
+        padding: 5px 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .date {
+          font-size: 12px;
+          color: #999;
+        }
+      }
     }
+    .right {
+      span,
+      span i {
+        font-size: 12px;
+        color: #333;
+      }
+      span {
+        margin-right: 5px;
+      }
+    }
+  }
+}
 </style>
