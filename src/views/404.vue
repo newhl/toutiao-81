94b28542dbac8f9621c2a484215093402feb01c2
hl <1813927324@qq.com>
2019-08-26 21:26:01 +0800

    404页面

---
+++ src/views/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-card class="not-fonud">
+
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .not-fonud{
+        width: 100%;
+        height: 100vh;
+        background-image: url('../assets/img/404.png');
+        background-size: cover
+    }
+</style>
